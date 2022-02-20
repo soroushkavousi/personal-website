67894618fe9279e9e973808eda477cd3e1dbfc97
Soroush Kavousi <soroushkavousi.me@gmail.com>
2022-02-20 10:24:34 +0330

    feat: Improve the navigation

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,7 +1,16 @@
 <template>
   <v-navigation-drawer permanent fixed app width="280" color="primary darken-2">
     <div class="fill-height d-flex flex-column justify-center">
-      <v-list>
+      <v-list nav>
+        <v-list-item class="d-flex justify-center ma-0">
+          <v-list-item-avatar class="mx-auto" size="160">
+            <v-img src="images/global/soroush.jpg" class="mx-auto"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-list-item class="d-flex justify-center mb-10">
+          <h1 class="secondary--text text--lighten-2">Soroush Kavousi</h1>
+        </v-list-item>
+
         <v-list-item-group v-model="selectedItemId">
           <v-list-item
             v-for="(item, i) in items"
@@ -11,9 +20,10 @@
             exact
             ripple
             :value="item.title"
+            class="mb-1"
           >
             <v-list-item-content
-              class="text-center font-weight-medium pa-2"
+              class="text-center font-weight-medium py-1"
               :class="{
                 'text--disabled': item.routeName !== $route.name,
               }"
--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,14 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img
-        src="images/global/soroush.jpg"
+      <h1 class="secondary--text text--lighten-2">Soroush Kavousi</h1>
+      <!-- src="images/global/soroush.jpg" -->
+      <!-- <img
+        src="~/assets/images/global/soroush.jpg"
         alt="Soroush Kavousi"
         class="mb-5"
         height="300"
-      />
-      <h1 class="secondary--text text--lighten-2">Soroush Kavousi</h1>
+      /> -->
     </v-col>
   </v-row>
 </template>
