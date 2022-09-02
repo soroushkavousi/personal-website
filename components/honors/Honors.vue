bd450bd032c679c126bce3de4b92197819044e9c
Soroush Kavousi <soroushkavousi.me@gmail.com>
2022-09-03 00:57:42 +0430

    refactor: Improve Honors breakpoints

--- components/honors/Honors.vue
+++ components/honors/Honors.vue
@@ -2,9 +2,11 @@
   <v-container fluid>
     <h1 class="accent--text text--lighten-2 text-h3 mb-14 ml-16"># HONORS</h1>
     <v-row justify="center" class="mb-16 mt-0">
-      <v-col xs="10" sm="10" md="10" lg="9">
-        <v-card color="primary" tile class="lighten-1 pa-16">
-          <v-card-text class="text-body-1 font-weight-light">
+      <v-col xl="9" lg="11" md="10" cols="12">
+        <v-card color="primary" tile class="lighten-1 py-16 px-14">
+          <v-card-text
+            class="text-md-body-1 text-body-2 font-weight-light pa-0"
+          >
             - Achieved top rank 0.6% in the nationwide
             <span class="font-weight-bold">University Entrance Exam</span>
             for Mathematics and Physics 2013<span
