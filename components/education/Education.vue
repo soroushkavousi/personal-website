a9a3cfdf5c523b949efd3cdf2c72704df15b5404
soroushkavousi <soroushkavousi.me@gmail.com>
2023-04-26 16:07:53 +0000

    refactor: Improve the education section

--- components/education/Education.vue
+++ components/education/Education.vue
@@ -3,7 +3,11 @@
     <h1 class="accent--text text--lighten-2 text-h3 mb-14 ml-16">
       # EDUCATION
     </h1>
-    <v-row justify="center">
+    <v-row
+      justify="center"
+      align="center"
+      :style="{ 'min-height': sectionMinHeight + 'px' }"
+    >
       <v-col xl="5" lg="7" md="8" sm="11" cols="12">
         <v-timeline dense class="ml-n11">
           <v-timeline-item
@@ -24,39 +28,41 @@
             >
               <v-card
                 tile
-                height="160"
+                :height="imageHeight"
                 color="secondary darken-1"
-                class="d-flex justify-center"
+                class="d-flex flex-column justify-center"
               >
-                <v-card
-                  tile
-                  flat
-                  color="secondary darken-1"
-                  class="d-flex flex-column justify-center"
-                  v-for="(image, i) in education.images"
-                  :key="i"
-                  :href="image.website"
-                >
-                  <v-img
-                    class="d-inline-block"
-                    width="100%"
-                    max-height="140"
-                    :src="image.src"
-                    light
-                    contain
-                  ></v-img>
-                </v-card>
+                <v-img
+                  class="d-inline-block"
+                  width="100%"
+                  :max-height="imageHeight - 15"
+                  :src="education.image"
+                  :href="education.website"
+                  light
+                  contain
+                ></v-img>
               </v-card>
-              <v-card-title class="text-lg-h6 text-body-1 font-weight-bold">
-                <p :class="`${textColor1}`">{{ education.title }}</p>
+              <v-card-title :class="`${textColor1}`" class="pa-8 pb-4">
+                <div class="text-lg-h6 text-body-2 font-weight-normal">
+                  <span class="font-weight-bold">{{ education.location }}</span
+                  ><span>, {{ education.country }}</span>
+                </div>
+                <div class="text-lg-body-2 text-caption font-weight-normal">
+                  {{ education.title }}
+                </div>
               </v-card-title>
               <v-card-text
-                class="text-sm-body-2 text-caption font-weight-light"
+                class="highlight pa-8 pt-0"
+                :class="{
+                  'highlight-lg': !$store.state.breakpoint.isMdOrDown,
+                }"
               >
-                <span
-                  v-html="education.location"
-                  :class="`${textColor2}`"
-                ></span>
+                <p
+                  class="mb-1"
+                  v-for="(highlight, i) in education.highlights"
+                  :key="i"
+                  v-html="'- ' + highlight"
+                ></p>
               </v-card-text>
             </v-card>
           </v-timeline-item>
@@ -70,6 +76,8 @@
 export default {
   data() {
     return {
+      sectionMinHeight: 700,
+      imageHeight: 190,
       color1Parts: {
         name: 'blue',
         type: 'darken-1',
@@ -81,35 +89,16 @@ export default {
       educations: [
         {
           date: '2013 - 2019',
+          location: 'Amirkabir University of Technology',
+          country: 'Iran',
+          website: 'https://ce.aut.ac.ir/en',
           title: 'B.Sc, Computer Engineering (Software Engineering)',
-          images: [
-            {
-              src: 'https://files.bitiano.com/logos/amirkabir-university-logo.png',
-              website: 'https://ce.aut.ac.ir/en',
-            },
+          image:
+            'https://files.bitiano.com/logos/amirkabir-university-logo.png',
+          highlights: [
+            'Thesis: <b>Part-of-speech tagging and text normalizing for informal Persian texts</b> using various methods such as the sequence-to-sequence deep learning model.',
+            '<a href="https://www.scimagoir.com/rankings.php?sector=Higher+educ.&country=IRN&ranking=Overall&area=1700" target="_blank">Top 3 high-ranked universities in the country</a>',
           ],
-          location: `<p class="ma-0"><a href="https://ce.aut.ac.ir/en" class="vit" target="_blank">Amirkabir University of Technology</a>, Tehran, Iran</p>
-            <p class="ma-0 ml-2">- <a href="https://www.usnews.com/education/best-global-universities/amirkabir-university-of-technology-aut-506266" target="_blank">Ranked 293th in Computer Science [U.S. News]</a></p>
-            <p class="ml-2">- <a href="https://www.scimagoir.com/rankings.php?sector=Higher+educ.&country=IRN&ranking=Overall&area=1700" target="_blank">Top 3 high-ranked universities in the country</a></p>`,
-        },
-        {
-          date: '2009 - 2013',
-          images: [
-            {
-              src: 'https://files.bitiano.com/logos/nodet-logo.png',
-              website:
-                'https://en.wikipedia.org/wiki/National_Organization_for_Development_of_Exceptional_Talents',
-            },
-            // {
-            //   src: 'https://files.bitiano.com/logos/salam-logo.webp',
-            //   website: 'https://yousefabad.salamsch.com/hschool',
-            // },
-          ],
-          title: 'High School Diploma, Mathematics and Physics',
-          location: `
-            <p class="ma-0">Shahid Beheshti (NODET) High School, Gonbad Kavus, Iran.</p>
-            <p><a href="https://en.wikipedia.org/wiki/National_Organization_for_Development_of_Exceptional_Talents" class="vit ml-2" target="_blank">- Affiliated with the National Organization for the Development of Exceptional Talents</a></p>
-            <p>Salam YousefAbad High School, Tehran, Iran.</p>`,
         },
       ],
     }
@@ -132,18 +121,29 @@ export default {
 </script>
 
 <style scoped>
-:deep(.vit) {
-  text-decoration: none;
-  color: var(--v-accent-lighten2) !important;
+.highlight {
+  font-size: 0.8rem !important;
+  font-weight: 300;
+  line-height: 1.45;
+  white-space: normal;
+  font-family: 'OpenSans' !important;
+  color: rgba(255, 255, 255, 0.6);
+}
+.highlight-lg {
+  font-size: 0.9rem !important;
 }
 
-:deep(p > a) {
-  text-decoration: none;
-  color: var(--v-secondary-base) !important;
+:deep(a) {
+  color: rgba(255, 255, 255, 0.85) !important;
+  font-weight: 600;
 }
 
-:deep(p > a:hover),
-:deep(p > a:active) {
-  color: var(--v-accent-lighten3) !important;
+:deep(a:hover),
+:deep(a:active) {
+  color: var(--v-accent-lighten4) !important;
+}
+
+:deep(b) {
+  color: rgba(255, 255, 255, 0.85) !important;
 }
 </style>
